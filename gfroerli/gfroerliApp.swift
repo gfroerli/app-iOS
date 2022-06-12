c34385fcc11c1115278101bedf90989e4a1fa797
makram95 <marc.kramer95@icloud.com>
2022-06-12 16:36:14 +0200

    Initial Commit

---
+++ gfroerli/gfroerliApp.swift
@@ -0,0 +1,17 @@
+//
+//  gfroerliApp.swift
+//  gfroerli
+//
+//  Created by Marc Kramer on 12.06.22.
+//
+
+import SwiftUI
+
+@main
+struct gfroerliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
